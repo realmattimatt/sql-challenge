25eec728771145f13ddb2d00b6f5ebaa2b4fd5ad
@mattimatt-wMdRJi <128356316+realmattimatt@users.noreply.github.com>
2024-12-02 04:53:57 -0600

    finished questions and notes.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,5 +1,7 @@
+-- Module 9 Challenge
 
 -- List the employee number, last name, first name, sex, and salary of each employee.
+
 SELECT 
     e.emp_no,
     e.last_name,
@@ -13,15 +15,17 @@ JOIN
 
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
+
 SELECT 
     e.last_name,
     e.first_name,
     e.hire_date
 FROM employees e
-WHERE (e.hire_date) = 1986;
+WHERE e.hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 
--- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+-- List the manager of each department along with their 
+--department number, department name, employee number, last name, and first name.
 
 SELECT
     dm.dept_no,
@@ -34,8 +38,9 @@ FROM dept_manager dm
     JOIN departments d ON dm.dept_no = d.dept_no;
 
     
+-- List the department number for each employee along with that 
+--employee’s employee number, last name, first name, and department name.
 
--- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.  
 SELECT
     de.dept_no,
     e.emp_no,
@@ -47,5 +52,49 @@ FROM dept_emp de
     JOIN departments d ON de.dept_no = d.dept_no;
 
 
+-- List first name, last name, and sex of each employee whose first name 
+--is Hercules and whose last name begins with the letter B.
+
+SELECT
+	e.first_name,
+	e.last_name,
+	e.sex
+FROM employees e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%'
 
 
+-- List each employee in the Sales department, including their employee number,
+--last name, and first name.
+
+SELECT 
+	d.dept_name,
+	e.emp_no,
+	e.last_name,
+	e.first_name
+FROM departments d
+JOIN dept_emp de ON de.dept_no = d.dept_no
+JOIN employees e ON e.emp_no = de.emp_no
+WHERE d.dept_name = 'Sales';
+
+
+-- List each employee in the Sales and Development departments,
+--including their employee number, last name, first name, and department name.
+
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+
+-- List the frequency counts, in descending order, of all the employee last names
+--(that is, how many employees share each last name).
+
+SELECT last_name, COUNT(*) AS lname_count
+FROM employees
+GROUP BY last_name
+ORDER BY lname_count DESC;
--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,7 +1,18 @@
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS titles;
+
+
 CREATE TABLE titles (
     title_id VARCHAR PRIMARY KEY,
     title VARCHAR NOT NULL
 );
+
+--SELECT * FROM titles ... ok
+--LIMIT 10; ok
 
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
@@ -18,11 +29,17 @@ CREATE TABLE departments (
     dept_name VARCHAR NOT NULL
 );
 
+-- SELECT * FROM departments ...ok
+-- LIMIT 10; ...ok
+
 CREATE TABLE dept_manager (
     emp_no INT REFERENCES employees(emp_no),
     dept_no VARCHAR REFERENCES departments(dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
+
+SELECT * FROM dept_manager
+LIMIT 10;
 
 CREATE TABLE dept_emp (
     emp_no INT REFERENCES employees(emp_no),
@@ -35,3 +52,21 @@ CREATE TABLE salaries (
     salary INT NOT NULL,
     PRIMARY KEY (emp_no)
 );
+
+
+SELECT 
+    e.emp_no,
+    e.last_name,
+    e.first_name,
+    e.sex,
+    s.salary
+FROM 
+    employees e
+JOIN 
+    salaries s ON e.emp_no = s.emp_no;
+
+
+-- SELECT * FROM dept_emp
+-- LIMIT 10;
+
+
