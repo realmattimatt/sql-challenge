e81ca7fcbd52a188bc7a8b448e60b764986333fc
@mattimatt-wMdRJi <128356316+realmattimatt@users.noreply.github.com>
2024-12-10 01:31:23 -0600

    Updated NOT NULL and composite keys.

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -7,45 +7,50 @@ DROP TABLE IF EXISTS titles;
 
 
 CREATE TABLE departments (
-    dept_no VARCHAR PRIMARY KEY,
-    dept_name VARCHAR NOT NULL
+    dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(100) NOT NULL
 );
 
 
 CREATE TABLE titles (
-    title_id VARCHAR PRIMARY KEY,
-    title VARCHAR NOT NULL
+    title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+    title VARCHAR(100) NOT NULL
 );
 
 
 CREATE TABLE employees (
-    emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR REFERENCES titles(title_id),
-    birth_date DATE,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR(1),
-    hire_date DATE
+    emp_no INT PRIMARY KEY NOT NULL,
+    emp_title_id VARCHAR(10),
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 
 CREATE TABLE salaries (
-    emp_no INT REFERENCES employees(emp_no),
-    salary INT NOT NULL,
-    PRIMARY KEY (emp_no)
+    emp_no INT,
+    salary DECIMAL(10, 2),
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no) ON DELETE CASCADE
 );
 
 
 CREATE TABLE dept_emp (
-    emp_no INT REFERENCES employees(emp_no),
-    dept_no VARCHAR REFERENCES departments(dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    emp_no INT,
+    dept_no VARCHAR(10),
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no) ON DELETE CASCADE,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
-
 CREATE TABLE dept_manager (
-	dept_no VARCHAR REFERENCES departments(dept_no),
-    emp_no INT REFERENCES employees(emp_no),
-    PRIMARY KEY (emp_no, dept_no)
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no) ON DELETE CASCADE,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
